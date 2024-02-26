76f060d47617f3526c0a7d47149ed81fac93600b
Jakub <jakubpachut@gmail.com>
2024-02-26 01:41:08 +0100

    finih another section

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const navMobile = document.querySelector('.nav__mobile')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
-
+const allNavItems = document.querySelectorAll('.nav__item')
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav__mobile--active')
+
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav__mobile--active');
+			navBtn.classList.remove('is-active');
+		})
+	})
 }
 
 const handleCurrentyear = () => {
